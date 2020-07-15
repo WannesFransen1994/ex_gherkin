4e2aa0bd8a770bfab260d55b7cf30d98df02d2c9
Wannes Fransen <wannes.fransen@ucll.be>
2020-07-15 10:28:17 +0200

    sample test

--- test/ex_gherkin_test.exs
+++ test/ex_gherkin_test.exs
@@ -3,22 +3,40 @@ defmodule ExGherkinTest do
   doctest ExGherkin
   require Logger
 
-  test "compare all testdata" do
-    files =
-      [File.cwd!(), "testdata", "good", "*.feature"]
-      |> Path.join()
-      |> Path.wildcard()
+  # test "compare all testdata" do
+  #   files =
+  #     [File.cwd!(), "testdata", "good", "*.feature"]
+  #     |> Path.join()
+  #     |> Path.wildcard()
 
-    Enum.each(files, fn path ->
-      correct_output = File.read!(path <> ".tokens")
-      tokenized_output = ExGherkin.tokenize(path)
-      result = correct_output == tokenized_output
+  #   Enum.each(files, fn path ->
+  #     correct_output = File.read!(path <> ".tokens")
+  #     tokenized_output = ExGherkin.tokenize(path)
+  #     result = correct_output == tokenized_output
 
-      if result == false do
-        Logger.warn("File #{path} is not being parsed correctly.")
-      end
+  #     if result == false do
+  #       Logger.warn("File #{path} is not being parsed correctly.")
+  #     end
 
-      assert result
-    end)
+  #     assert result
+  #   end)
+  # end
+
+  test "sampletest source correctly structured" do
+    path = "testdata/good/minimal.feature"
+    opts = ["--no-pickles", "--predictable-ids", "--no-ast"]
+
+    result =
+      ExGherkin.gherkin_from_path(path, opts)
+      |> ExGherkin.print_messages("ndjson")
+      |> Enum.map(&Jason.encode!(&1))
+      |> Enum.join("\n")
+
+    result = result <> "\n"
+
+    decent_result = File.read!(path <> ".source.ndjson")
+    File.write!("DIFF_ME", result)
+    File.write!("DIFF_ME_RESULT", decent_result)
+    assert result == decent_result
   end
 end
