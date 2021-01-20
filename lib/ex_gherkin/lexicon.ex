9f6e37033584840141a121e511a131f1bf8fdcc8
Wannes Fransen <wannes.fransen@ucll.be>
2021-01-20 19:24:12 +0100

    update

--- lib/ex_gherkin/lexicon.ex
+++ lib/ex_gherkin/lexicon.ex
@@ -1,6 +1,7 @@
 defmodule ExGherkin.Gherkin.Lexicon do
   @moduledoc false
-  @default_lexicon_path [File.cwd!(), "priv", "gherkin_languages.json"] |> Path.join()
+  @default_lexicon_path [File.cwd!(), :code.priv_dir(:ex_gherkin), "gherkin_languages.json"]
+                        |> Path.join()
   @feature_keywords ["feature"]
   # @scen_keywords ["scenario"]
   # @scen_outline_keywords ["scenarioOutline"]
